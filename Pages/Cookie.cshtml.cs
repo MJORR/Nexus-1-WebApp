22e24f822b4fc98d255af10bff390ed0332658af
Magnus <magnusjackorr@gmail.com>
2022-03-01 15:28:03 +0000

    Pages

---
+++ Pages/Cookie.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Nexus_1_WebApp.Pages;
+
+public class CookieModel : PageModel
+{
+    private readonly ILogger<CookieModel> _logger;
+
+    public PrivacyModel(ILogger<CookieModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
