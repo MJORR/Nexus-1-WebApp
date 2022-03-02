14a7f824cfce2e0bddcfda13277c6fa24ec3c65c
Magnus <magnusjackorr@gmail.com>
2022-03-02 11:35:58 +0000

    layout updates

---
+++ Pages/Cookies.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Nexus_1_WebApp.Pages
+{
+    public class CookiesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
